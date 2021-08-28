e315e5901919448a83746bce4be388f89890973c
Miha <xd383727@gmail.com>
2021-08-28 13:05:35 +0300

    form download

--- mainBooks/forms.py
+++ mainBooks/forms.py
@@ -2,20 +2,6 @@ from django import forms
 from django.core.validators import FileExtensionValidator
 from .models import Books, Autor, Categories
 
-class BooksForm(forms.ModelForm):
-    book = forms.FileField(label='Загрузите книгу', validators=[
-        FileExtensionValidator(allowed_extensions=('pdf', 'fb2', 'mobi', 'epab', 'djvu'))
-    ],error_messages={'invalid_extension': 'Этот формат не поддерживается'}, widget=forms.FileInput)
-    name = forms.CharField(label='Название книги', max_length=50,widget=forms.TextInput())
-    autors = 'AutorForm'
-    categories = 'CategoriesForms'
-    description = forms.CharField(label='Описание',empty_value=True ,widget=forms.TextInput())
-
-    class Meta:
-        model = Books
-        fields = '__all__'
-
-
 class AutorForm(forms.ModelForm):
     first_name = forms.CharField(max_length=20,widget=forms.TextInput())
     second_name = forms.CharField(max_length=20, widget=forms.TextInput())
@@ -30,3 +16,17 @@ class CategoriesForms(forms.ModelForm):
     class Meta:
         model = Categories
         fields = '__all__'
+
+
+
+class BooksForm(forms.ModelForm):
+    book = forms.FileField(label='Загрузите книгу', validators=[
+        FileExtensionValidator(allowed_extensions=('pdf', 'fb2', 'mobi', 'epab', 'djvu'))
+    ],error_messages={'invalid_extension': 'Этот формат не поддерживается'}, widget=forms.FileInput)
+    name = forms.CharField(label='Название книги', max_length=50,widget=forms.TextInput())
+    description = forms.CharField(label='Описание',empty_value=True ,widget=forms.TextInput())
+
+    class Meta:
+        model = Books
+        fields = '__all__'
+
