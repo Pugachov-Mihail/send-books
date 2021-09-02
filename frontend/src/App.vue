c1b53e7fad0985e55d31a2f503518994a9df9071
Miha <xd383727@gmail.com>
2021-09-02 23:28:20 +0300

    create nav panel on vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav-panel
+  v-for="menu in NavMenu"
+  :key="menu.hello"
+  >
+    <h1>{{ menu.hello }}</h1>
+  </nav-panel>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import NavPanel from './NavPanel'
+import './reset.css'
+import './style.css'
+
 
 export default {
   name: 'App',
+  data() {
+    return {
+      NavMenu: {
+        hello: "hello"
+      }
+    }
+  },
   components: {
-    HelloWorld
+    'nav-panel': NavPanel
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ frontend/src/NavPanel.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+     <nav class="nav">
+        <div class="logo">
+            <h2>
+                <span class="get">Get</span>
+                <span class="book">Book</span>
+            </h2>
+        </div>
+        <div class="main">
+            <ul>
+                <li><a>Главная</a></li>
+                <li>Каталог</li>
+                <li><a href="{% url 'office' %}"> Привет, </a><a href="{% url 'login' %}">Войти</a></li>
+                <li><a href="{% url 'createAvtor' %}">Загрузить</a></li>
+            </ul>
+        </div>
+        <button class="regist"><a href="{% url 'logout' %}">Выйти</a><a href="{% url 'registration' %}">Регистрация</a></button>
+    </nav>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            hello: 'hello'
+        }
+    },
+}
+</script>
+
+
