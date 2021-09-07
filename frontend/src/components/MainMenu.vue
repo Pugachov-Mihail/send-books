9fa67ac5ee0a2a61c884cd21f2d4da21c1f9d680
Miha <xd383727@gmail.com>
2021-09-07 13:46:21 +0300

    create mainMenu

---
+++ frontend/src/components/MainMenu.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="main">
+    <div class="main_block">
+      <img src="{{ imgBook }}" alt="Book" />
+      <h1 class="heading">{{ book.name }}</h1>
+      <h3 class="autor">{{ book.autor }}</h3>
+      <p class="description"></p>
+      <button class="btn"></button>
+      <div class="reit">
+        <h2>Рейтинг</h2>
+        <span class="starts">&#9734;&#9734;&#9734;&#9734;&#9734;</span>
+        <button class="download">
+          <a href="" download="">Скачать</a>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  imgBook: "/src/assets/book.jpg",
+};
+</script>
